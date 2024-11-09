be84a51b48510153584dce0295206728004d62ea
Michael <mow26@icloud.com>
2024-11-09 01:46:20 -0500

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/se_project_react/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
